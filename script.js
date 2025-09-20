3af998bf919379d0fd6ac80ecd80ea642ff952ac
RiteshMaury97 <riteshmaury97@gmail.com>
2025-09-20 21:10:11 +0530

    half completed commit message

---
+++ script.js
@@ -0,0 +1,7 @@
+//   <!-- Setup and start animation! -->
+     <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    var typed = new Typed('#element', {
+      strings: ['Graphic Desginer', 'Web Developer', 'Freelancer'],
+      typeSpeed: 50,
+    });
+    // <!-- End animation! -->
